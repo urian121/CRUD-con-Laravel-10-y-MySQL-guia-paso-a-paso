07ec15829fb056e4683c3c88a80e2b9289bf7e68
URIAN VIERA <urian1213viera@gmail.com>
2024-03-06 20:46:07 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use App\Http\Controllers\EmpleadosController;
 Route::get('/', [EmpleadosController::class, 'index'])->name('home');
 Route::post('/store', [EmpleadosController::class, 'store'])->name('myStore');
 Route::get('/empleado/show/{id}', [EmpleadosController::class, 'show'])->name('myShow');
-Route::delete('/empleado/delete/{id}', [EmpleadosController::class, 'destroy'])->name('mydestroy');
+Route::delete('/empleado/delete/{id}', [EmpleadosController::class, 'destroy'])->name('myDestroy');
 Route::get('/empleado/edit/{id}', [EmpleadosController::class, 'edit'])->name('myEdit');
 Route::put('/empleado/update/{id}', [EmpleadosController::class, 'update'])->name('myUpdate');
